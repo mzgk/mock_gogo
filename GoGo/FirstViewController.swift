8287e7792e711c825fb75e3bf9dae3e2616cb797
mzgk <mzgkworks@gmail.com>
2017-09-22 11:33:05 +0900

    Initial Commit

---
+++ GoGo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GoGo
+//
+//  Created by 水垣岳志 on 2017/09/22.
+//  Copyright © 2017年 mzgkworks. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
