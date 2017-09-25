05ac8e5759734f9c438fb695613808f16906b986
mzgk <mzgkworks@gmail.com>
2017-09-25 12:52:17 +0900

    add: CMPedometorを利用して歩数・距離・時間を計測して表示

--- GoGo/FirstViewController.swift
+++ GoGo/FirstViewController.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import CoreMotion
 
 class FirstViewController: UIViewController {
 
+    let myPedometor = CMPedometer()
+    weak var timer: Timer!
+
+    @IBOutlet weak var labelSteps: UILabel!
+    @IBOutlet weak var labelDistance: UILabel!
+    @IBOutlet weak var labelTime: UILabel!
+    @IBOutlet weak var buttonExecute: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +30,51 @@ class FirstViewController: UIViewController {
     }
 
 
-}
+    @IBAction func tapExecuteButton(_ sender: UIButton) {
+        if sender.titleLabel?.text == "Start" {
+            labelSteps.text = "0"
+            labelDistance.text = "0"
+            labelTime.text = "00:00"
 
+            startCounting()
+            buttonExecute.setTitle("Stop", for: UIControlState())
+        }
+        else {
+            myPedometor.stopUpdates()
+            buttonExecute.setTitle("Start", for: UIControlState())
+        }
+    }
+
+    func startCounting() {
+        if CMPedometer.isPedometerEventTrackingAvailable() {
+            myPedometor.startUpdates(from: Date(), withHandler: { (data: CMPedometerData?, error: Error?) in
+                DispatchQueue.main.async {
+                    // errorがある場合はリターン
+                    if error != nil {
+                        print("ERROR: \(String(describing: error?.localizedDescription))")
+                        return
+                    }
+                    // dataがnil場合はリターン
+                    guard data != nil else {
+                        print("ERROR: data = nil")
+                        return
+                    }
+                    let lengthFormatter = LengthFormatter()
+                    let steps = data?.numberOfSteps
+                    let distance = data?.distance?.doubleValue
+                    let time = data?.endDate.timeIntervalSince((data?.startDate)!)
+
+                    self.labelSteps.text = steps?.stringValue
+                    self.labelDistance.text = lengthFormatter.string(fromMeters: distance!)
+
+                    // 秒数から時間を算出
+                    let formatter = DateComponentsFormatter()
+                    formatter.unitsStyle = .positional
+                    formatter.allowedUnits = [.minute, .hour, .second]
+                    self.labelTime.text = formatter.string(from: time!)
+                }
+            })
+        }
+    }
+
+}
